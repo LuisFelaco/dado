434889abd370f0c770d6be3314e10da85518dc39
LuisFelaco <45235114+LuisFelaco@users.noreply.github.com>
2018-11-21 19:09:22 +0100

    Update Dadi.py

--- Dadi.py
+++ Dadi.py
@@ -1,12 +1,13 @@
 import random
-Prova=1000000
-i=0
-lista=[0,0,0,0,0,0]
+
+Prova = 1000
+i = 0
+lista = [0,0,0,0,0,0]
+
 def dado ():
 	return random.randrange (1,7)
 for i in range(Prova):
-	risultato=dado()
+	risultato = dado()
 	lista[risultato-1]+=1
-	#print (risultato, end=' ')
 
 print (lista)
